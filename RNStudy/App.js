51ac2fbb0d7019f5984528f3ffd755066a5c7925
wuyongchao <wuyongchao@pingougou.cn>
2018-04-20 16:44:43 +0800

    TextInput输入文本

--- RNStudy/App.js
+++ RNStudy/App.js
@@ -9,18 +9,25 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+    TextInput
 } from 'react-native';
 
 export default class App extends Component<{}> {
+    constructor(props){
+        super(props);
+        this.state={text:''};
+    }
   render() {
     return (
         // row column
         <View style={{flex:1,flexDirection:'column',justifyContent:'space-between', alignItems:'center'}} >
             <View style={{width:50,height:50,backgroundColor:'blue'}}/>
             <View style={{width:100,height:100,backgroundColor:'red'}}/>
-            {/*<View style={{flex:1,backgroundColor:'blue'}}/>*/}
-            {/*<View style={{flex:2,backgroundColor:'red'}}/>*/}
+            <TextInput style={{height:40,width:200}} placeholder='hello world la la' onChangeText={(text)=>this.setState({text})}/>
+            <Text style={{padding: 10, fontSize: 42}}>
+                {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+            </Text>
         </View>
 
     );
