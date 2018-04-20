aa2b599c2d4fff87c4f7df0b48ecb2dc5ad6ae72
wuyongchao <wuyongchao@pingougou.cn>
2018-04-20 15:41:36 +0800

    对于需要改变的数据，我们需要使用state。constructor中初始化state在需要修改时调用setState方法

--- RNStudy/App.js
+++ RNStudy/App.js
@@ -12,21 +12,37 @@ import {
   View
 } from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { showText: true };
+
+        // 每1000毫秒对showText状态做一次取反操作
+        setInterval(() => {
+            this.setState(previousState => {
+                return { showText: !previousState.showText };
+            });
+        }, 1000);
+    }
+
     render() {
+        //根据当前showText的值决定是否显示text内容
+        let display = this.state.showText ? this.props.text : ' ';
         return (
-            <Text>Hello {this.props.name}!</Text>
+            <Text>{display}</Text>
         );
+      
     }
 }
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={{alignItems: 'center'}}>
-          <Greeting name='张三' />
-          <Greeting name='李四' />
-          <Greeting name='王五' />
-      </View>
+        <View>
+            <Blink text='I love to blink' />
+            <Blink text='Yes blinking is so great' />
+            <Blink text='Why did they ever take this out of HTML' />
+            <Blink text='Look at me look at me look at me' />
+        </View>
     );
   }
 }
