6dd5be0590a6966434b143547ef1dd6196996de0
wuyongchao <wuyongchao@pingougou.cn>
2018-04-20 11:59:57 +0800

    简单样式修改

--- RNStudy/App.js
+++ RNStudy/App.js
@@ -24,7 +24,7 @@ export default class App extends Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          大家好，我是永超！
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
