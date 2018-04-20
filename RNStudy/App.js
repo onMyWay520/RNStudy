74bbcb8b52f3775379c53fb154a194ffca857a90
wuyongchao <wuyongchao@pingougou.cn>
2018-04-20 15:31:23 +0800

    this.props 定义组件

--- RNStudy/App.js
+++ RNStudy/App.js
@@ -9,49 +9,25 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View,
-    Image
+  View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: '从今天开始编程RN\n' +
-    '好好学习，早日成为大神',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
+class Greeting extends Component {
+    render() {
+        return (
+            <Text>Hello {this.props.name}!</Text>
+        );
+    }
+}
 export default class App extends Component<{}> {
   render() {
-      let pic = {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-      };
     return (
-        <Image source={pic} style={{width: 193, height: 110}} />
+      <View style={{alignItems: 'center'}}>
+          <Greeting name='张三' />
+          <Greeting name='李四' />
+          <Greeting name='王五' />
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-    study: {
-        fontSize: 24,
-        textAlign: 'center',
-        margin: 5,
-    },
-  instructions: {
-    textAlign: 'center',
-    color: 'red',
-    marginBottom: 5,
-      fontSize:18
-  },
-});
