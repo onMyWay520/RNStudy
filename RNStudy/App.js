7876691c89d1061fcf548b2ca51ea64852ccf548
wuyongchao <wuyongchao@pingougou.cn>
2018-04-20 16:59:30 +0800

    scrollView 上下滑动

--- RNStudy/App.js
+++ RNStudy/App.js
@@ -10,25 +10,28 @@ import {
   StyleSheet,
   Text,
   View,
-    TextInput
+    TextInput,
+    ScrollView,
+    Image
 } from 'react-native';
 
 export default class App extends Component<{}> {
-    constructor(props){
-        super(props);
-        this.state={text:''};
-    }
   render() {
     return (
-        // row column
-        <View style={{flex:1,flexDirection:'column',justifyContent:'space-between', alignItems:'center'}} >
-            <View style={{width:50,height:50,backgroundColor:'blue'}}/>
-            <View style={{width:100,height:100,backgroundColor:'red'}}/>
-            <TextInput style={{height:40,width:200}} placeholder='hello world la la' onChangeText={(text)=>this.setState({text})}/>
-            <Text style={{padding: 10, fontSize: 42}}>
-                {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-            </Text>
-        </View>
+        <ScrollView>
+            <Text style={{fontSize:30}}>Scroll me </Text>
+            <Image source={require('./img/favicon.png')} />
+            <Image source={require('./img/favicon.png')} />
+            <Image source={require('./img/favicon.png')} />
+            <Image source={require('./img/favicon.png')} />
+            <Image source={require('./img/favicon.png')} />
+            <Text style={{fontSize:50}}>If you like</Text>
+            <Image source={require('./img/favicon.png')} />
+            <Image source={require('./img/favicon.png')} />
+            <Image source={require('./img/favicon.png')} />
+            <Image source={require('./img/favicon.png')} />
+            <Image source={require('./img/favicon.png')} />
+        </ScrollView>
 
     );
   }
