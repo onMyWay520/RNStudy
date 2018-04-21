240069b9a717c52978bfdcd95606e4c3d30641dd
wuyongchao <wuyongchao@pingougou.cn>
2018-04-21 15:03:53 +0800

    Tabbabr探究，有bug

--- RNStudy/App.js
+++ RNStudy/App.js
@@ -7,12 +7,43 @@
 import React, { Component } from 'react';
 import {
    StyleSheet,
+    Text,
+    View
  } from 'react-native';
-import SimpleApp from './Main'
-
+// import SimpleApp from './Main'
+import TabBar from "react-native-nav-tabbar";
 export default class App extends Component<{}> {
   render() {
-    return <SimpleApp />;
+    // return <SimpleApp />;
+      return (
+          <View style={styles.container}>
+              <TabBar>
+                  <TabBar.Item
+                      icon={require('./images/Home.png')}
+                      selectedIcon={require('./images/HomeActive.png')}
+                      title="Home"
+                  >
+                      <View style={styles.textContent}>
+                          <Text style={{fontSize: 18}}>Home</Text>
+                      </View>
+                  </TabBar.Item>
+                  <TabBar.Item>
+                      <View style={styles.textContent}>
+                          <Text style={{fontSize: 18}}>Friend</Text>
+                      </View>
+                  </TabBar.Item>
+                  <TabBar.Item
+                      icon={require('./images/My.png')}
+                      selectedIcon={require('./images/MyActive.png')}
+                      title="Me"
+                  >
+                      <View style={styles.textContent}>
+                          <Text style={{fontSize: 18}}>Me</Text>
+                      </View>
+                  </TabBar.Item>
+              </TabBar>
+          </View>
+      );
 
   }
 }
@@ -22,5 +53,10 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    textContent: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 });
