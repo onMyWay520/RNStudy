513da41504d0440f9a1bc73a21c04de652885ae2
wuyongchao <wuyongchao@pingougou.cn>
2018-07-12 21:02:50 +0800

    倒计时功能提交部分

--- RNStudy/src/screen/rootApp.js
+++ RNStudy/src/screen/rootApp.js
@@ -1,93 +1,44 @@
 import React, {Component} from 'react';
-import Swiper from 'react-native-swiper';
-var ReactNative = require('react-native');
-var {
-    View,
-    StyleSheet,
-    Image,
 
-} = ReactNative;
-var Dimensions = require('Dimensions');
-var {width, height} = Dimensions.get('window');
+import LCCountDownButton from "./LCCountDownButton"
+// var ReactNative = require('react-native');
+// var {
+//     View,
+//     StyleSheet,
+//     Image,
+//
+// } = ReactNative;
 
-var banner = [
-    'http://oweq6in8r.bkt.clouddn.com/about.jpg',
-    'http://oweq6in8r.bkt.clouddn.com/liuliangqiu.jpg',
-    'http://oweq6in8r.bkt.clouddn.com/rn.jpeg',
-];
 export default class rootApp extends Component {
 
     render() {
 
         return (
+            <LCCountDownButton frameStyle={{top:44 * 3 + 4,right:10,width:120,height:36,position:'absolute'}}
+                               beginText='获取验证码'
+                               endText='再次获取验证码'
+                               count={10}
+                               // pressAction={this._countDownButtonPressed()}
+                               changeWithCount={(count)=> count + 's后重新获取'}
+                               id='register'
+                               ref={(e)=>{this.countDownButton=e}}
+            />
 
-             <Swiper  showsButtons={false} height={220} width={width} horizontal={true} autoplay autoplayTimeout={2}
-                      dot={<View style={{backgroundColor:'rgba(0,0,0,.5)', width: 8, height: 8,borderRadius: 4}} />}
-                      activeDot={<View style={{backgroundColor: 'blue', width: 8, height: 8, borderRadius: 4}} />}
-                      paginationStyle={{
-                          top: -350, centerX:self
-                      }}
-                    >
-                 <View style={styles.slide1} >
-                     <Image
-                         style={styles.image}
-                         source={{uri:banner[0]}}
-                     />
-
-                 </View>
-                 <View style={styles.slide1} >
-                     <Image
-                         style={styles.image}
-                         source={{uri:banner[1]}}
-                     />
-                 </View>
-                 <View style={styles.slide1} >
-                     <Image
-                         style={styles.image}
-                         source={{uri:banner[2]}}
-                     />
-                 </View>
-
-            </Swiper>
-
-        );
+        )
 
 
+    }
+    _countDownButtonPressed(){
+//触发倒计时
+        this.countDownButton.startCountDown();
+
+//请求发送验证码
+        fetch('请求验证码')
+            .then()
+            .catch()
     }
 
 
 
 }
-
-// 样式
-var  styles = StyleSheet.create({
-    wrapper: {
-    },
-    slide1: {
-        // flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width:width,
-        height:200
-
-    },
-    slide2: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'orange'
-    },
-    slide3: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#92BBD9'
-    },
-
-    image: {
-        width:width,
-        flex: 1
-    }
-
-    });
 
