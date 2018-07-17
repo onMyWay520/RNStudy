f845e2199b2ec052755d863ba575eea6bb28885d
wuyongchao <wuyongchao@pingougou.cn>
2018-07-17 18:37:49 +0800

    轮播加进去

--- RNStudy/src/screen/SearchView.js
+++ RNStudy/src/screen/SearchView.js
@@ -2,15 +2,24 @@ import React, {Component} from 'react';
 import Search from '../common/search';
 // import Util from '../common/util';
 // import ServiceURL from '../common/Service';
+import Swiper from 'react-native-swiper';
 import  {
     StyleSheet,
     Text,
     View,
-
     ScrollView,
     Image,
     TouchableOpacity,
+    Dimensions
 } from 'react-native';
+// var Dimensions = require('Dimensions');
+var {width, height} = Dimensions.get('window');
+
+var banner = [
+    'http://oweq6in8r.bkt.clouddn.com/about.jpg',
+    'http://oweq6in8r.bkt.clouddn.com/liuliangqiu.jpg',
+    'http://oweq6in8r.bkt.clouddn.com/rn.jpeg',
+];
 
 export default class  SearchView extends Component {
     static navigationOptions = {
@@ -34,7 +43,7 @@ export default class  SearchView extends Component {
         return (
             <ScrollView style={styles.flex_1}>
 
-                <View style={[styles.search, styles.row]}>
+                  <View style={[styles.search, styles.row]}>
 
                     <View style={styles.flex_1}>
                         <Search placeholder="请输入电影的名称" onChangeText={this._changeText.bind(this)}/>
@@ -45,6 +54,7 @@ export default class  SearchView extends Component {
                     </TouchableOpacity>
 
                 </View>
+                {this._renderSwiper()}
 
 
             </ScrollView>
@@ -66,18 +76,41 @@ export default class  SearchView extends Component {
 
     // 搜素按钮点击后触发，获取数据
     _search() {
+        alert('1234')
         // this._getData();
     }
 
 
+    _renderSwiper() {
+       return(<Swiper  showsButtons={false} height={220} width={width} horizontal={true} autoplay autoplayTimeout={2}
+                       dot={<View style={{backgroundColor:'rgba(0,0,0,.5)', width: 8, height: 8,borderRadius: 4}} />}
+                       activeDot={<View style={{backgroundColor: 'blue', width: 8, height: 8, borderRadius: 4}} />}
+                       paginationStyle={{
+                           top: -350, centerX:self
+                       }}>
+           <View style={styles.slide1} >
+               <Image
+                   style={styles.image}
+                   source={{uri:banner[0]}}
+               />
+
+           </View>
+           <View style={styles.slide1} >
+               <Image
+                   style={styles.image}
+                   source={{uri:banner[1]}}
+               />
+           </View>
+       </Swiper>)
+    }
 };
 
 const styles = StyleSheet.create({
 
     flex_1: {
         flex: 1,
-        marginTop: 5,
-        height: 50,
+        marginTop: 10,
+
     },
 
     search: {
@@ -111,6 +144,19 @@ const styles = StyleSheet.create({
         width: 200,
         marginLeft: 10,
     },
+    slide1: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width:width,
+        height:200
+
+    },
+    image: {
+        width:width,
+        flex: 1
+    }
+
+
 
 
 });
